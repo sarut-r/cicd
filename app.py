2b056034d704db1a0b430273c0c6b04237ed3a5c
felcona <r.sarutksip@gmail.com>
2021-04-01 14:56:02 +0700

    [R.SARUT] Added add function

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
+
+
+def add(a, b):
+    return a + b
+
+
 if __name__ == "__main__":
     print('CICD')
+
+    try:
+        assert add(1, 1) == 2, "Should be 2"
+        print('success')
+    except:
+        print('error')
