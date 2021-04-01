a01ebf1cc6fc46a782d6652f32d4b1420d4d561d
felcona <r.sarutksip@gmail.com>
2021-04-01 15:03:44 +0700

    no message

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-
-
-def add(a, b):
-    return a + b
+from lib import utils
 
 
 if __name__ == "__main__":
     print('CICD')
 
     try:
-        assert add(1, 1) == 2, "Should be 2"
+        assert utils.add(1, 1) == 2, "Should be 2"
         print('success')
     except:
         print('error')
